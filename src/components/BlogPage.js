de204ceea00f733b9f7a20c49615b875d2ad6645
Sree Teja <msreeteja24@gmail.com>
2025-11-26 12:29:53 +0530

    Added the components of Blog page and added the Blog page component in App.js

---
+++ src/components/BlogPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BlogPost from "./BlogPost";
+
+export default function BlogPage() {
+  return (
+    <div>
+      <h3> Blog </h3>
+      <BlogPost></BlogPost>
+    </div>
+  );
+}
---
+++ src/components/BlogPost.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./Comment";
+
+export default function BlogPost() {
+  return (
+    <div>
+      <h2> Example post Title</h2>
+      <p> This is an example post content</p>
+      <Comment></Comment>
+    </div>
+  );
+}
