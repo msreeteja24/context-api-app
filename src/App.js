7cf03c22fdd42f2fc0d20eb1d4c8edac871ae038
Sree Teja <msreeteja24@gmail.com>
2025-11-26 12:35:05 +0530

    Created a userContext provier in the App.js and wrapped it around the Blog page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import BlogPage from "./components/BlogPage";
+import { useContext } from "react";
+import UserInfoContext from "./context/UserInfoContext";
 
 function App() {
+  const userInfo = { user: "Admin", isAdmin: "true" };
   return (
     <div className="App">
-      <BlogPage></BlogPage>
+      <UserInfoContext.Provider value={userInfo}>
+        <BlogPage></BlogPage>
+      </UserInfoContext.Provider>
     </div>
   );
 }
