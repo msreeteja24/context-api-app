f1b8eec13efd7edd6aecfd95e185122c1958c508
Sree Teja <msreeteja24@gmail.com>
2025-11-26 12:40:10 +0530

    Access the context in the component where it is required(comment.js)

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useContext } from "react";
+import UserInfoContext from "../context/UserInfoContext";
 
 export default function Comment() {
+  //To get access to the context - we use the useContext hook
+  const { user, isAdmin } = useContext(UserInfoContext);
   return (
     <div>
-      <p>Logged in as ...</p>
+      <p>Logged in as {user}</p>
+      {isAdmin && <button>Edit Comment</button>}
     </div>
   );
 }
